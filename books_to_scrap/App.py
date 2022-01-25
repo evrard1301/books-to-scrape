64e6373642befdd7e541af3689b70377f25a6765
bog <bog@gozmail.net>
2022-01-25 17:53:44 +0100

    Add: progressbar.

--- books_to_scrap/App.py
+++ books_to_scrap/App.py
@@ -2,7 +2,7 @@ import concurrent
 import os
 import threading
 import requests
-
+from tqdm import tqdm
 from book import BookImageDownloader
 from books_to_scrap.CategoryCollector import CategoryCollector
 from category import CategoryFetcher, CategoryExporter
@@ -27,9 +27,11 @@ class App:
 
         categories = []
         self.categories = categories
+        progress = tqdm(total=len(category_urls), desc='Load categories')
 
         def export_category(url, output_dir):
             category = self.fetch_category(url)
+            progress.update(1)
             categories.append(category)
             self.export_category(category, output_dir)
 
@@ -37,21 +39,25 @@ class App:
             for url in category_urls:
                 executor.submit(export_category, url, output_dir)
         executor.shutdown(wait=True)
+        progress.close()
 
     def run_images(self, output_dir):
         session = self.session
         mutex = threading.Lock()
 
+        progress = tqdm(total=997, desc='Load images')
+
         def dl_image(the_book):
             dl = BookImageDownloader(the_book, session, output_dir)
             dl.exec()
+            progress.update(1)
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=self.max_workers) as executor:
             for category in self.categories:
                 executor.map(dl_image, category.books)
-                #for book in category.books:
-                #    executor.submit(dl_image, book)
+
         executor.shutdown(wait=True)
+        progress.close()
 
     def fetch_category(self, url):
         fetcher = CategoryFetcher(url, self.session)
